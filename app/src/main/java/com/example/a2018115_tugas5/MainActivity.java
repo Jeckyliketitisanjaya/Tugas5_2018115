ce86b78e78d63c633e0f49e0053b66b9ccac0b1b
Edo <jeckylike9@gmail.com>
2022-11-09 02:44:38 +0700

    2018115

---
+++ app/src/main/java/com/example/a2018115_tugas5/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.a2018115_tugas5;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    // tugas recycle view
+    RecyclerView RecylerView;
+    String s1[], s2[],s3[];
+    int images[] = {R.drawable.geprek,R.drawable.kecap,R.drawable.kunyit};
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abut;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // tugas recycle view
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecylerView = findViewById(R.id.recyclerView);
+        s1 = getResources().getStringArray(R.array.Makanan);
+        s2 = getResources().getStringArray(R.array.Deskripsi);
+        s3 = getResources().getStringArray(R.array.Star);
+        MakananAdapter appAdapter = new MakananAdapter(this, s1, s2, s3, images);
+        RecylerView.setAdapter(appAdapter);
+        LinearLayoutManager layoutManager  = new LinearLayoutManager(MainActivity.this,LinearLayoutManager.HORIZONTAL,false);
+        RecylerView.setLayoutManager(layoutManager);
+        RecylerView.setItemAnimator(new DefaultItemAnimator());
+
+
+        dl = (DrawerLayout) findViewById(R.id.dl);
+        abut = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
+        abut.setDrawerIndicatorEnabled(true);
+        dl.addDrawerListener(abut);
+        abut.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_profile) {
+                    Intent a = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(a);
+                } else if (id == R.id.nav_reviews) {
+                    Intent a = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(a);
+                } else if (id == R.id.nav_settings) {
+                    Intent a = new Intent(MainActivity.this, MainActivity3.class);
+                    startActivity(a);
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return abut.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+}
